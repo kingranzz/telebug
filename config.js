9b33dac5041b7bc4048d80ef0249b51a99745488
ranzselebew <sbuat537@gmail.com>
2024-12-25 19:27:58 +0800

    bugwa

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  BOT_TOKEN: "7877141767:AAFgP2Mkq8QHV5nShVLBQEgZCT7ovN_yal8", // Token bot Telegram
+  OWNER_ID: "6669561065", // ID pemilik bot
+  allowedGroupIds: [-1002401306074, -1002361556670, -1002386380830, -1002415460020, -4514397992], // ID grup yang diizinkan
+};
